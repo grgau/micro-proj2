4033186dfe005ad3fbf28a34144e44cb7e043e90
pferracini <pedro.ferracini@sjrp.unesp.br>
2017-12-08 22:41:22 -0200

    Funcao de delay, nao sei como testar se isso ta dando certo

--- projeto2.X/programa.asm
+++ projeto2.X/programa.asm
@@ -1,3 +1,9 @@
+; Especificacao do projeto: 
+;   Escrever um programa que a cada 2s, leia um valor na porta B, representando um inteiro de
+; 16 bits, e um valor de 5 bits na porta A. Seu programa deve então dividir o valor de 16
+; bits pelo de 5 bits, duas vezes, retornando o resultado na porta B.
+; resultado = B/(A^2)
+
 ; Alunos:
 ; Gabriel Bueno
 ; Paulo Paim
@@ -14,26 +20,62 @@ porta   equ	05	; Precisa verificar se aqui é 05 e portb é 06 mesmo
 portb   equ	06
 trisa   equ	05
 trisb   equ	06
+
+; Variaveis
+divisor		equ 05	; Valor de 5 bits lido na porta
+parte1dividendo	equ 08	; Primeiros 8 bits do valor lido na portb
+parte2dividendo	equ 08	; Segunda parte de 8 bits lida na portb
+resultado	equ 08	; Resultado da divisao B/(A^2)
+contadordelay1	equ 11	; Contador de delay auxiliar
+contadordelay2	equ 12	; Contador de delay auxiliar
    
     org 00	    ; Comecando programa em 0x00
-
-   
-; Variaveis
-divisor	equ 05
-parte1dividendo	equ 08
-parte2dividendo	equ 08
     
 ; Inicializacao
 start	bsf status,5	    ; Seleciona o banco 1
     movlw   1F		    ; Move 1 para w
     movwf   trisb	    ; Move valor de w para portb (definindo portb como entrada)
     movwf   trisa	    ; Move valor de w para porta (definindo porta como entrada)
-    bcf	status,5	    ; Seleciona o banco 0
+    bcf	status,5	    ; Seleciona o banco 0    
+    ; Inicio programa
     
+loop
+    movf    porta,0	    ; Move valor da porta para registrador 0 (registrador w)
+    movwf   divisor	    ; Move valor de w para variavel divisor
+    clrw		    ; Limpa o registrador w
+    movf    portb,0	    ; Move valor da portb para o registrador 0 (registrador w)
+    movwf   parte1dividendo ; Move valor de w para variavel parte1dividendo
+    clrf    porta	    ; Limpa o valor de porta
+    clrf    portb	    ; Limpa valor de portb
+    			    ; Condicional se vai ler mais 8 bits na portb
+    movf    portb,0	    ; Move valor da portb para registrador 0 (registrador w)
+    movwf   parte2dividendo ; Move valor de w para variavel parte2dividendo
+
+    call dividir	    ; Chama subrotina de divisao
+    call delay		    ; Chama subrotina de realizar delay de 2 segundos
     
+    goto loop		    ; Volta para inicio do programa
     
+dividir
+    clrw
+    return
+
+delay				; Subrotina de delay, acredito que esteja fazendo 2 segundos de delay
+    movlw   D'4000'		; Move valor 1000 para w
+    movwf   contadordelay2	; Move valor de w para contadordelay2
+
+outer movlw   D'1999'		; Move valor 1999 para w		(1 ciclo)
+    movwf   contadordelay1	; Move valor de w para contador 1	(1 ciclo)
     
-; Inicio programa
+inner nop			;					(1 ciclo)
+    nop				;					(1 ciclo)
+    decfsz  contadordelay1, 1	; Decrementa em 1 o contadordelay1	(1 ciclo)
+    goto inner			;					(2 ciclos)
     
+    decfsz  contadordelay2, 1	; Decrementa em 1 o contadordelay2	(1 ciclo)
+    goto outer			;					(2 ciclos)
+				; OBS: ((5 ciclos laco interno * 1999) + 5 ciclos laco externo) * 4000 = 40000000 ciclos
+				; OBS2: PIC 16F873A possui clock de 20MHZ => 1 ciclo = 5*10^-8 segundos, portanto 40000000 ciclos = 2 segundos 
+    return
     
     end
